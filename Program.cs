47dbba3ca3036496ab273c49a2f42283982fb9bc
saquibamjad189 <50143889+saquibamjad189@users.noreply.github.com>
2019-04-30 15:59:01 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,69 +1,90 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+/* ========================================================================
+ * Copyright (c) 2005-2019 The OPC Foundation, Inc. All rights reserved.
+ *
+ * OPC Foundation MIT License 1.00
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * The complete license agreement can be found here:
+ * http://opcfoundation.org/License/MIT/1.00/
+ * ======================================================================*/
+
+using System;
+using System.Windows.Forms;
+using Opc.Ua;
+using Opc.Ua.Configuration;
+using Opc.Ua.Server.Controls;
 using System.Threading.Tasks;
-using VDS.RDF;
-using VDS.RDF.Parsing;
-using VDS.RDF.Storage;
-using VDS.RDF.Query;
 
-
-namespace ConsoleApp2
+namespace Quickstarts.FKHBserver
 {
-    class Program
+    static class Program
     {
-        public static String getGrandfather(String grandchild )  // sparql query to get the grandfather
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            SparqlRemoteEndpoint endpoint = new SparqlRemoteEndpoint(new Uri("http://localhost:8890/sparql"), "http://localhost:8890/test");
-            ////Make a SELECT query against the Endpoint
-            SparqlResultSet results = endpoint.QueryWithResultSet("SELECT ?grandParent WHERE { <http://bedrock/"+grandchild+"> <http://bedrock/hasParent> ?o.  ?o <http://bedrock/hasParent> ?grandParent}");
-            String res = "";
-            foreach (SparqlResult result in results)
-            { 
-                res += result.ToString().Split('=')[result.ToString().Split('=').Length -1].Split('/')[result.ToString().Split('=')[result.ToString().Split('=').Length - 1].Split('/').Length-1];
-            }
-            return res;
-        }
+            // Initialize the user interface.
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
 
-        public static IGraph addTrippleToFHKB(IGraph FHKB , String subject, String predicate, String objec)
-        {
-            INode sub = FHKB.CreateUriNode(UriFactory.Create("http://bedrock/"+subject));
-            INode pred = FHKB.CreateUriNode(UriFactory.Create("http://bedrock/"+predicate));
-            INode obj = FHKB.CreateUriNode(UriFactory.Create("http://bedrock/"+objec));
-            Triple t = new Triple(sub, pred, obj);
-            FHKB.Assert(t);
-            //foreach (Triple k in g.Triples)
-            //{
-            //    Console.WriteLine(k.ToString());
-            //}
-            return FHKB;
-        }
-        public static void Main(string[] args)
-        {
-            //VirtuosoManager manager = new VirtuosoManager("localhost", VirtuosoManager.DefaultPort, VirtuosoManager.DefaultDB, "dba", "dba");
-            //IGraph g = new Graph();
-            //manager.LoadGraph(g,new Uri("http://localhost:8890/test"));
-            //INode ali = g.CreateUriNode(UriFactory.Create("http://bedrock/Ali"));
-            //INode hasParent = g.CreateUriNode(UriFactory.Create("http://bedrock/hasParent"));
-            //INode mullar = g.CreateUriNode(UriFactory.Create("http://bedrock/Mullar"));
-            //Triple t = new Triple(ali, hasParent, mullar);
-            ////g.Assert(t);
-            //foreach (Triple k in g.Triples)
-            //{
-            //    Console.WriteLine(k.ToString());
-            //}
-            //Console.WriteLine(g.BaseUri);
-            ////manager.SaveGraph(g);          // to save updates happen to the graph
-            ////Console.WriteLine(g.IsEmpty);
-            //Console.ReadLine();
-            while (true)
+            ApplicationInstance.MessageDlg = new ApplicationMessageDlg();
+            ApplicationInstance application = new ApplicationInstance();
+            application.ApplicationType   = ApplicationType.Server;
+            application.ConfigSectionName = "Quickstarts.FKHBserver";
+
+            try
             {
-                Console.WriteLine("Please enter The name of the grandchild: ");
-                String grandchildName = Console.ReadLine();
-                Console.WriteLine(getGrandfather(grandchildName));
-            }
 
+                // load the application configuration.
+                application.LoadApplicationConfiguration(false).Wait();
+
+                // check the application certificate.
+                bool certOk = application.CheckApplicationInstanceCertificate(false, 0).Result;
+                if (!certOk)
+                {
+                    throw new Exception("Application instance certificate invalid!");
+                }
+
+                // start the server.
+                application.Start(new FKHBserver()).Wait();
+
+                // run the application interactively.
+                Application.Run(new ServerForm(application));
+            }
+            catch (Exception e)
+            {
+                ExceptionDlg.Show(application.ApplicationName, e);
+            }
         }
+    }
+
+    /// <summary>
+    /// The <b>FKHBserver</b> namespace contains classes which implement a Quickstart Server.
+    /// </summary>
+    /// <exclude/>
+    [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    public class NamespaceDoc
+    {
     }
 }
